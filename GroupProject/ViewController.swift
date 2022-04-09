4af29b71703a18e915ec3d63f14b0476d87d3fba
Ji Wang <wj42421999@gmail.com>
2022-04-08 22:46:25 -0400

    Init Xcode project

---
+++ GroupProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroupProject
+//
+//  Created by Ji Wang on 4/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
